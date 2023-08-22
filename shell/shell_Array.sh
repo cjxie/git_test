efd5640695b6f45c0946972a22b837d4590ee5ef
cjxie <xieshijie399@gmail.com>
2023-08-22 14:25:38 -0400

    Array & @/*

---
+++ shell/shell_Array.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+my_array=(A B "C" D)
+for i in ${my_array};
+do
+	echo ${i}
+done
+
+echo "4th element is ${my_array[3]}"
+
+
+echo "关联数组(hash map)"
+
+declare -A site=(["google"]="www.google.com" ["runoob"]="www.runoob.com" ["taobao"]="www.taobao.com")
+
+echo ${site[*]}
+echo "Keys of array: ${!site[*]}"
+echo "# of elements: ${#site[*]}"
+site["123"]="www.123.com"
+
+echo ${site[@]}
+echo "Keys of array: ${!site[@]}"
+echo "# of elements: ${#site[@]}"
--- shell/shell_Variable.sh
+++ shell/shell_Variable.sh
@@ -8,7 +8,23 @@ echo "Excuteable file name: $0"
 echo "1st parameter: $1"
 echo "2nd parameter: $2"
 
+separator="******************"
+echo -e "\n${separator}"
 
+echo "# of parameters: $#"
+echo "传递的参数作为一个字符串显示<\$* or \$@>: $*"
+echo -e "\n${separator}"
 
+echo "-- \S* demo --"
+for i in "$*";
+do
+	echo $i
+done
 
+echo "-- \S@ demo --"
+for i in "$@";
+do
+        echo $i
+done
 
+echo -e "\n${separator}"
