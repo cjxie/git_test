1c4184c27d459aca4f91215124bdda94322f60fa
cjxie <xieshijie399@gmail.com>
2023-08-31 16:20:45 -0400

    opticalFlow learning

---
+++ camera/opticalFlow_tutorial/algorithms/CMakeFiles/OpticalFlow.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpticalFlow.dir/main.cpp.o"
+  "OpticalFlow"
+  "OpticalFlow.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OpticalFlow.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
